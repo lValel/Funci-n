99926afe79e9c61dec05b5428701b824e20f881c
lValel <165740731+lValel@users.noreply.github.com>
2024-04-23 17:08:12 -0500

    Create funcion.py

---
+++ funcion.py
@@ -0,0 +1,42 @@
+import discord
+import random
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+    
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('!programación'):
+        await message.channel.send("Estamos en programación de Python Pro")
+    elif message.content.startswith('$generarcontraseña'):
+        contrasenia=gen_pass(5)
+        await message.channel.send (f"Tu contraseña es: {contrasenia}")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTIyOTU5ODIxNzE4ODkzMzc3Mw.GKdUvH.ybaLMghGyFHHPQnMnHrVoUzbn_soAvf8zX5PBM")
